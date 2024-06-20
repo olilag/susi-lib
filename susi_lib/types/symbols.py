c1f518f98b750c2ffaeb4ec212be2f21c7ad3d5c
Oliver Lago <oliver.oli.lago@gmail.com>
2024-06-20 22:00:55 +0200

    Add conversion methods

--- susi_lib/types/symbols.py
+++ susi_lib/types/symbols.py
@@ -1,5 +1,9 @@
 from susi_lib.types import Braille
 from susi_lib.types import Morse
+from susi_lib.types import NumberSystems
+from susi_lib.types import Semaphore
+
+
 class Symbols:
     def __init__(self, characters: str):
         if not isinstance(characters, str):
@@ -11,3 +15,18 @@ class Symbols:
 
     def to_morse(self) -> Morse:
         return Morse(self.__characters)
+
+    def to_number_systems(self) -> NumberSystems:
+        return NumberSystems(self.__characters)
+
+    def to_semaphore(self) -> Semaphore:
+        return Semaphore(self.__characters)
+
+    def __getitem__(self, item) -> "Symbols":
+        return Symbols(self.__characters[item])
+
+    def __str__(self):
+        return self.__characters
+
+    def __len__(self):
+        return len(self.__characters)
