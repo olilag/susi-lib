dc9495325be5f7fadba940c83b6fe20fdb55ea43
Oliver Lago <oliver.oli.lago@gmail.com>
2024-06-26 16:37:07 +0200

    Add regex tests

---
+++ susi_lib/test/test_regex.py
@@ -0,0 +1,28 @@
+import unittest
+from susi_lib.regex import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_create_regex(self):
+        self.assertEqual(
+            create_regex(
+                ("abc", Selection.NONE), ("def", Selection.INVERT), ("", Selection.ANY)
+            ).get_pattern(),
+            "^[abc][^def][.]$",
+        )
+        self.assertEqual(
+            create_regex(length=5, letters="auto", invert=False).get_pattern(),
+            "^[auto]{5}$",
+        )
+        self.assertEqual(
+            create_regex(length=(5, 10), letters="auto", invert=False).get_pattern(),
+            "^[auto]{5,10}$",
+        )
+        self.assertEqual(
+            create_regex(length=5, letters="auto", invert=True).get_pattern(),
+            "^[^auto]{5}$",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
