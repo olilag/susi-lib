72ebfd146020172f1c62f63607ca855a20fe1be2
Oliver Lago <oliver.oli.lago@gmail.com>
2024-09-21 11:12:52 +0200

    Rewrite Dictionary to not use class getitem

--- src/susi_lib/dictionary.py
+++ src/susi_lib/dictionary.py
@@ -16,7 +16,15 @@ class _Mapping(TypedDict):
     ZT_a: str
 
 
-class Dictionary:
+class _DictMeta(type):
+    def __getitem__(cls, item: Literal["PM", "PM_a", "S", "S_a", "ZT", "ZT_a"]) -> Path:
+        p = cls.download_dir / cls.file_mapping[item]
+        if not p.is_file():
+            cls.download_file(cls.file_mapping[item])
+        return p
+
+
+class Dictionary(metaclass=_DictMeta):
     """Class for quick access to SuŠi dictionaries.
 
     If the wanted dictionary is not downloaded, it automatically downloads it and saves it
@@ -37,11 +45,11 @@ class Dictionary:
     - "ZT_a" => "zakladne_tvary_ascii.txt",
     """
 
-    __host_url = (
+    host_url = (
         "https://raw.githubusercontent.com/olilag/susi-lib/refs/heads/master/assets/"
     )
     download_dir = user_cache_path("susi-lib", False)
-    __file_mapping: _Mapping = {
+    file_mapping: _Mapping = {
         "PM": "podstatne_mena.txt",
         "PM_a": "podstatne_mena_ascii.txt",
         "S": "slovnik.txt",
@@ -51,11 +59,5 @@ class Dictionary:
     }
 
     @classmethod
-    def __download_file(cls, name: str) -> None:
-        urlretrieve(cls.__host_url + name, cls.download_dir / name)
-
-    def __class_getitem__(cls, item: Literal["PM", "PM_a", "S", "S_a"]) -> Path:
-        p = cls.download_dir / cls.__file_mapping[item]
-        if not p.is_file():
-            cls.__download_file(cls.__file_mapping[item])
-        return p
+    def download_file(cls, name: str) -> None:
+        urlretrieve(cls.host_url + name, cls.download_dir / name)
